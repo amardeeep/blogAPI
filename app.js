f50efd51bf87f14e9c27a05572051c32cb42fdbd
Deetsy <amardeepdeepak2016@gmail.com>
2024-10-21 17:31:43 +0530

    Add environment variable port

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require("express");
+const dotenv = require("dotenv");
+dotenv.config();
+const PORT = process.env.PORT || 3000;
 const app = express();
 app.get("/", (req, res) => {
   res.send("Moshi Moshi");
 });
-app.listen(3000, () => {
-  "Listening on Port 3000";
+app.listen(PORT, () => {
+  console.log(`Listening on PORT ${PORT}`);
 });
